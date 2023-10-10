42295d49444872f0a8a545c4607459fa70cca32b
Jani Giannoudis <jani@giannoudis.ch>
2023-10-10 18:53:12 +0200

    added CultureTool.cs

---
+++ Client.Test/CultureTool.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using PayrollEngine.Client.Model;
+
+namespace PayrollEngine.Client.Test
+{
+    internal static class CultureTool
+    {
+        internal static CultureInfo GetTenantCulture(Tenant tenant)
+        {
+            var culture = CultureInfo.DefaultThreadCurrentCulture ?? CultureInfo.InvariantCulture;
+            if (!string.IsNullOrWhiteSpace(tenant.Culture) &&
+                !string.Equals(culture.Name, tenant.Culture))
+            {
+                culture = new CultureInfo(tenant.Culture);
+            }
+            return culture;
+        }
+    }
+}
